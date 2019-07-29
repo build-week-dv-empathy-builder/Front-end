62358f8a00fa7fa15539b3de59aa1a716d111da6
Travis Little <kingtrav@gmail.com>
2019-07-28 18:01:34 -0600

    home changes

--- dv-empathy/src/components/Home.js
+++ dv-empathy/src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <h3>VI</h3>
+            <h3>Vision</h3>
             <p>We envision a world where survivors have sustaining income, savings and credit with which to build wealth and the resources to support individual, intergenerational and community healing.</p>
             <br />
             <p>We envision a world where harm-doers are held financially accountable and survivors are not saddled with the cost of being harmed.</p>
