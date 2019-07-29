094b76effc61729bf0634b4480cbdb7672411f0e
Travis Little <kingtrav@gmail.com>
2019-07-28 18:01:25 -0600

    submit data working
    Co-authored-by: Danielle Baxter <danibaxx@hotmail.com>

--- dv-empathy/src/components/Home.js
+++ dv-empathy/src/components/Home.js
@@ -8,11 +8,13 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <p>
-            On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
-            </p>
-        <Link to="/calculator">Start Calculator</Link>
-        
+            <h3>VI</h3>
+            <p>We envision a world where survivors have sustaining income, savings and credit with which to build wealth and the resources to support individual, intergenerational and community healing.</p>
+            <br />
+            <p>We envision a world where harm-doers are held financially accountable and survivors are not saddled with the cost of being harmed.</p>
+            <br /> 
+            <p>We envision a world where gender-based violence receives the attention necessary to address it as a systemic issue.</p>
+                <Link to="/calculator">Start Calculator</Link>   
         </div>
     )
 }
--- dv-empathy/src/components/InputRelocationCosts.js
+++ dv-empathy/src/components/InputRelocationCosts.js
@@ -17,10 +17,65 @@ class InputRelocationCosts extends React.Component {
     }
 
     handleSubmit = (event) => {
-        // event.preventDefault()
-        // axios.post('')
-        //     .then()
-        //     .catch()
+        const headers = {
+            authorization: localStorage.getItem('token')    
+        }
+        const payload = {
+            //has vehicle in general?
+            //general data
+            "current_location": "81401",
+            "timestamp": "20181026",
+            "desired_relocation": "87401",
+            "sex": "male",
+            "relationship_status": "married",
+            "orientation": "straight",
+            "age": "28",
+            "race": "white",
+            "safe_status": "yes",
+            "employed": "no",
+            "partner_employed": "yes",
+            "children": "yes",
+            "personal_savings": "1000",
+            "individual_income": "500",
+        
+            //child care
+            //personal budget
+            "transportation": "50",
+            "food": "200",
+            "health_care": "0",
+            "car_loans": "0",
+            "personal_loans": "0",
+            "personal_other": "200",
+            "personal_budget_total": "450",
+        
+            //relocation budget
+            "travel_costs": "50",
+            "rental_deposit": "500",
+            "utility_connection": "100",
+            "storage_unit": "50",
+            "rent": "600",
+            "car_rental": "0",
+            "cell_phone": "100",
+            "moving_truck": "300",
+            "mental_health": "0",
+            "income_loss": "0",
+            "additional_security": "100",
+            "relocation_other": "200",
+            "relocation_budget_total": "2000",
+        
+            //calculated results
+            "calculated_difference": "1450", 
+        }
+
+        event.preventDefault()
+        axios.post('https://dv-empathy-builder.herokuapp.com/api/insert', payload, { headers })
+            .then((res) => {
+                console.log("response", res)
+                console.log("data sent", res.data.message)
+            })
+            .catch((err) => {
+                console.log("error sending data", err)
+            })
     }
 
     render() {
