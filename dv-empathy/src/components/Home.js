328725895470e8107bd354624130b5fc689d2695
danibaxx <danibaxx@hotmail.com>
2019-07-28 21:01:08 -0600

    updated InputRelocationCosts.js with form inputs and labels

--- dv-empathy/src/components/Home.js
+++ dv-empathy/src/components/Home.js
@@ -9,8 +9,9 @@ function Home() {
         <div>
             <h1>Home</h1>
             <br />
-            <h3>From our CEO:</h3>
+            <h2>From our CEO:</h2>
             <p>We live in a society that encourages survivors to leave, but tells them that once they do, they’re on their own to figure the rest out. Do we actually want to end domestic violence in this country? Because if we do, we need to widen our understanding of the problem, and dare to dream of a society in which survivors have the resources to support their healing.</p>
+            <br />
         <Link to="/calculator">Start Calculator</Link>
         
         </div>
