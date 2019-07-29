88ce3010c3e8c1b442b8a1ce5026e8ad26d53e7c
danibaxx <danibaxx@hotmail.com>
2019-07-29 17:36:46 -0600

    updated code within InputRelocationCosts.js to be easier to read

--- dv-empathy/src/components/InputRelocationCosts.js
+++ dv-empathy/src/components/InputRelocationCosts.js
@@ -45,62 +45,122 @@ class InputRelocationCosts extends React.Component {
                 <Form onSubmit={ this.handleSubmit }>
 
                     <Label for="travel_costs">Travel Cost(s): </Label>
-                    <Input type="number" name="travel_costs" value={ travel_costs } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="travel_costs" 
+                        value={ travel_costs } 
+                        onChange={ this.handleChange }
+                    />
 
                     <br />
 
                     <Label for="rental_deposit">Rental Deposit: </Label>
-                    <Input type="number" name="rental_deposit" value={ rental_deposit } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="rental_deposit" 
+                        value={ rental_deposit } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="utility_connection">Utility Connection: </Label>
-                    <Input type="number" name="utility_connection" value={ utility_connection } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="utility_connection" 
+                        value={ utility_connection } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="storage_unit">Storage Unit: </Label>
-                    <Input type="number" name="storage_unit" value={ storage_unit } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="storage_unit" 
+                        value={ storage_unit } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="rent">Rent: </Label>
-                    <Input type="number" name="rent" value={ rent } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="rent" 
+                        value={ rent } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="car_rental">Car Rental: </Label>
-                    <Input type="number" name="car_rental" value={ car_rental } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="car_rental" 
+                        value={ car_rental } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="cell_phone">Cell Phone: </Label>
-                    <Input type="number" name="cell_phone" value={ cell_phone } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="cell_phone" 
+                        value={ cell_phone } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="moving_truck">Moving Truck: </Label>
-                    <Input type="number" name="moving_truck" value={ moving_truck } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="moving_truck" 
+                        value={ moving_truck } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="mental_health">Mental Health: </Label>
-                    <Input type="number" name="mental_health" value={ mental_health } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="mental_health" 
+                        value={ mental_health } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="income_loss">Income Loss: </Label>
-                    <Input type="number" type="number" name="income_loss" value={ income_loss } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="income_loss" 
+                        value={ income_loss } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="additional_security">Additional Security: </Label>
-                    <Input type="number" type="number" name="additional_security" value={ additional_security } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="additional_security" 
+                        value={ additional_security } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
 
                     <Label for="relocation_other">Other Relocation Cost(s): </Label>
-                    <Input type="number" name="relocation_other" value={ relocation_other } onChange={ this.handleChange } />
+                    <Input 
+                        type="number" 
+                        name="relocation_other" 
+                        value={ relocation_other } 
+                        onChange={ this.handleChange } 
+                    />
 
                     <br />
                     <Button type="submit">Submit</Button>
