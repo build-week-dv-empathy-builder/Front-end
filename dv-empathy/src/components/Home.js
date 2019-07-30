17247f797851cf186443fac0d6cdf8fd18f520f0
danibaxx <danibaxx@hotmail.com>
2019-07-29 20:22:23 -0600

    added img to Home.js, created img folder and home.css, updated img to fit app size

--- dv-empathy/src/components/Home.js
+++ dv-empathy/src/components/Home.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import { Button } from 'reactstrap'
-import { Link, Route } from 'react-router-dom'
-import CalculatorPage from './CalculatorPage';
-import InputCost from './InputCost';
+import { Link } from 'react-router-dom'
+
+import '../home.css';
+
+import LawStatue from '../Images/law_statue.png';
+
 
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <br />
+            <br />
+            <br />
+            <img src={ LawStatue } className="lady justice" alt="Lady Justice" />
+            <br />
             <br />
             <h3>From our CEO:</h3>
             <p>We live in a society that encourages survivors to leave, but tells them that once they do, they’re on their own to figure the rest out. Do we actually want to end domestic violence in this country? Because if we do, we need to widen our understanding of the problem, and dare to dream of a society in which survivors have the resources to support their healing.</p>
+            <br />
         <Link to="/calculator">Start Calculator</Link>
         
         </div>
