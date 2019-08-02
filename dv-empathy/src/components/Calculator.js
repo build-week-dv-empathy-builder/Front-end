eed15dda71458c438859d9369e7805ff92c13817
danibaxx <danibaxx@hotmail.com>
2019-08-01 20:57:47 -0600

    pulled from master didn't work, switched to master branch, did new branch, added import

--- dv-empathy/src/components/Calculator.js
+++ dv-empathy/src/components/Calculator.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import { Button, ListGroup, ListGroupItem } from 'reactstrap'
+import { Button, Table } from 'reactstrap'
 import appDataStructure from '../appDataStructure' // these are the object properties the APP uses to iterate over object properties
 
 
