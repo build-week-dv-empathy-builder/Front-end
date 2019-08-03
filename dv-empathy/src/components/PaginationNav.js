3ec6efeb593be69ab38f039ba25fe4630c02aa8f
Travis Little <kingtrav@gmail.com>
2019-08-03 11:09:13 -0600

    improvements to paginationnav.js

--- dv-empathy/src/components/PaginationNav.js
+++ dv-empathy/src/components/PaginationNav.js
@@ -8,28 +8,28 @@ function PaginationNav(props) {
             <Pagination size="sm" id="pagination-nav">
                 <PaginationItem>
                     {props.current === 'demographics' ? // disable back button if demographics is showing (which is first)
-                    <Button disabled size="sm" id="app-nav-button">{`${"<"}`}</Button> :
+                    <Link to="#"><Button disabled size="sm" id="app-nav-button">{`${"<"}`}</Button></Link> :
                     <Link to={props.prev} onClick={props.saveData}><Button size="sm" id="app-nav-button">{`${"<"}`}</Button></Link>}
                 </PaginationItem>
                 <PaginationItem>
                     {props.current === "demographics" ? 
-                    <Button disabled size="sm" id="app-nav-button">Your Demographics</Button> :
+                    <Link to="#"><Button disabled size="sm" id="app-nav-button">Your Demographics</Button></Link> :
                     <Link to="/calculator/demographics" onClick={props.saveData}><Button size="sm" id="app-nav-button">Your Demographics</Button></Link>
                     }
                 </PaginationItem>
                 <PaginationItem>
                     {props.current === "personal-budget" ?
-                    <Button disabled size="sm" id="app-nav-button">Personal Budget</Button> :
-                    <Link to="/calculator/personal-budget" onClick={props.saveData}><Button size="sm" id="app-nav-button">Personal Budget</Button></Link>}
+                    <Link to="#"><Button disabled size="sm" id="app-nav-button">Your Personal Budget</Button></Link> :
+                    <Link to="/calculator/personal-budget" onClick={props.saveData}><Button size="sm" id="app-nav-button">Your Personal Budget</Button></Link>}
                 </PaginationItem>
                 <PaginationItem>
                     {props.current ==="relocation-budget" ?
-                    <Button disabled size="sm" id="app-nav-button">Your Relocation Costs</Button> :
+                    <Link to="#"><Button disabled size="sm" id="app-nav-button">Your Relocation Costs</Button></Link> :
                     <Link to="/calculator/relocation-costs" onClick={props.saveData}><Button size="sm" id="app-nav-button">Your Relocation Costs</Button></Link>}
                 </PaginationItem>
                 <PaginationItem>
                     {props.current === 'relocation-budget' ? // disable next button if it's the relcation budget showing (which is last)
-                        <Button disabled size="sm" id="app-nav-button">{`${">"}`}</Button> :
+                        <Link to="#"><Button disabled size="sm" id="app-nav-button">{`${">"}`}</Button></Link> :
                         <Link to={props.next} onClick={props.saveData}>
                             <Button size="sm" id="app-nav-button">{`${">"}`}</Button>
                         </Link>}
