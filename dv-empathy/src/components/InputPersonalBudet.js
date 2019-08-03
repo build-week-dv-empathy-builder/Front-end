780c9b6983b7dcebf5835af865d5ea26b378da83
Travis Little <kingtrav@gmail.com>
2019-08-03 12:59:20 -0600

    form field sizing

--- dv-empathy/src/components/InputPersonalBudet.js
+++ dv-empathy/src/components/InputPersonalBudet.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { 
     Input, 
     Label, 
-    Form, 
+    Form,
+    FormGroup,
     UncontrolledPopover, 
     PopoverHeader, 
     PopoverBody,
-    Button,
 } from 'reactstrap'
-import { Link } from 'react-router-dom'
 import PaginationNav from './PaginationNav'
 
 class InputPersonalBudget extends React.Component {
@@ -57,115 +56,103 @@ class InputPersonalBudget extends React.Component {
                 <article>
                     <p>Please provide some basic information about your personal finances. This information will be anonymous and will not be sold or distributed to 3rd parties. Freefrom.org will use your information to better provide resources and assistance to those in need.</p>
                 </article>
-                <Form className="personal-budget-form">
-                    <Label for="individual_income">Individual Income: </Label>
-                    <Input 
-                        id="IndividualIncome"
-                        type="number" 
-                        name="individual_income" 
-                        value={ individual_income } 
-                        onChange={ this.handleChange }
-                        onWheel={this.handleScroll}
-                    />
-
-                    <UncontrolledPopover trigger="focus" placement="bottom" target="IndividualIncome">
-                    <PopoverHeader>Individual Income:</PopoverHeader>
-                    <PopoverBody>Any form of Income at a job, Side-job, Personal Projects that bring in extra income, Etc.</PopoverBody>
-                    </UncontrolledPopover>
-
-                    <br />
-
-                    <Label for="personal_savings">Personal Savings: </Label>
-                    <Input 
-                        type="number"
-                        name="personal_savings" 
-                        value={ personal_savings } 
-                        onChange={ this.handleChange }
-                        onWheel={this.handleScroll}
-                    />
-
-                    <br />
-
-                    <Label for="transportation">Transportation: </Label>
-                    <Input 
-                        id="Transportation"
-                        type="number"
-                        name="transportation" 
-                        value={ transportation } 
-                        onChange={ this.handleChange }
-                        onWheel={this.handleScroll} 
-                    />
-
-                    <UncontrolledPopover trigger="focus" placement="bottom" target="Transportation">
-                    <PopoverHeader>Transportation:</PopoverHeader>
-                    <PopoverBody>Any and all expenses needed for transportation.
-                    <br />
-                    Includes: Bus or train pass, Any other form of public transportation, Etc.</PopoverBody>
-                    </UncontrolledPopover>
-
-                    <br />
-
-                    <Label for="food">Food: </Label>
-                    <Input 
-                        id="Food"
-                        type="number"
-                        name="food" 
-                        value={ food } 
-                        onChange={ this.handleChange }
-                        onWheel={this.handleScroll}
-                    />
-
-                    <UncontrolledPopover trigger="focus" placement="bottom" target="Food">
-                    <PopoverHeader>Food:</PopoverHeader>
-                    <PopoverBody>Includes: Groceries, Dining out, Fast food, Etc.</PopoverBody>
-                    </UncontrolledPopover>
-
-                    <br />
-
-                    <Label for="health_care">Health Care: </Label>
-                    <Input 
-                        type="number"
-                        name="health_care" 
-                        value={ health_care } 
-                        onChange={ this.handleChange }
-                        onWheel={this.handleScroll}
-                    />
-
-                    <br />
-
-                    <Label for="car_loans">Car Loan(s): </Label>
-                    <Input 
-                        type="number"
-                        name="car_loans" 
-                        value={ car_loans } 
-                        onChange={ this.handleChange }
-                        onWheel={this.handleScroll}
-                    />
-
-                    <br />
-
-                    <Label for="personal_loans">Personal Loan(s): </Label>
-                    <Input 
-                        type="number"
-                        name="personal_loans" 
-                        value={ personal_loans } 
-                        onChange={ this.handleChange }
-                        onWheel={this.handleScroll} 
-                    />
-
-                    <br />
-
-                    <Label for="personal_other">Other Personal Expenses: </Label>
-                    <Input
-                        type="number"
-                        name="personal_other" 
-                        value={ personal_other } 
-                        onChange={ this.handleChange }
-                        onWheel={this.handleScroll}
-                    />
-
-                    <br />
-                    
+                <Form className="personal-budget form">
+                    <FormGroup>
+                        <Label for="individual_income">Individual Income: </Label>
+                        <Input 
+                            id="IndividualIncome"
+                            type="number" 
+                            name="individual_income" 
+                            value={ individual_income } 
+                            onChange={ this.handleChange }
+                            onWheel={this.handleScroll}
+                        />
+                        <UncontrolledPopover trigger="focus" placement="bottom" target="IndividualIncome">
+                            <PopoverHeader>Individual Income:</PopoverHeader>
+                            <PopoverBody><p>Any form of Income at a job, Side-job, Personal Projects that bring in extra income, Etc.</p></PopoverBody>
+                        </UncontrolledPopover>                        
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="personal_savings">Personal Savings: </Label>
+                        <Input 
+                            type="number"
+                            name="personal_savings" 
+                            value={ personal_savings } 
+                            onChange={ this.handleChange }
+                            onWheel={this.handleScroll}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="transportation">Transportation: </Label>
+                        <Input 
+                            id="Transportation"
+                            type="number"
+                            name="transportation" 
+                            value={ transportation } 
+                            onChange={ this.handleChange }
+                            onWheel={this.handleScroll} 
+                        />
+                        <UncontrolledPopover trigger="focus" placement="bottom" target="Transportation">
+                            <PopoverHeader>Transportation:</PopoverHeader>
+                            <PopoverBody><p>Any and all expenses needed for transportation.</p>
+                            <p>Includes: Bus or train pass, Any other form of public transportation, Etc.</p></PopoverBody>
+                        </UncontrolledPopover>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="food">Food: </Label>
+                        <Input 
+                            id="Food"
+                            type="number"
+                            name="food" 
+                            value={ food } 
+                            onChange={ this.handleChange }
+                            onWheel={this.handleScroll}
+                        />
+                        <UncontrolledPopover trigger="focus" placement="bottom" target="Food">
+                        <PopoverHeader>Food:</PopoverHeader>
+                        <PopoverBody><p>Includes: Groceries, Dining out, Fast food, Etc.</p></PopoverBody>
+                        </UncontrolledPopover>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="health_care">Health Care: </Label>
+                        <Input 
+                            type="number"
+                            name="health_care" 
+                            value={ health_care } 
+                            onChange={ this.handleChange }
+                            onWheel={this.handleScroll}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="car_loans">Car Loan(s): </Label>
+                        <Input 
+                            type="number"
+                            name="car_loans" 
+                            value={ car_loans } 
+                            onChange={ this.handleChange }
+                            onWheel={this.handleScroll}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="personal_loans">Personal Loan(s): </Label>
+                        <Input 
+                            type="number"
+                            name="personal_loans" 
+                            value={ personal_loans } 
+                            onChange={ this.handleChange }
+                            onWheel={this.handleScroll} 
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="personal_other">Other Personal Expenses: </Label>
+                        <Input
+                            type="number"
+                            name="personal_other" 
+                            value={ personal_other } 
+                            onChange={ this.handleChange }
+                            onWheel={this.handleScroll}
+                        />
+                    </FormGroup>  
                 </Form>
                 <PaginationNav saveData={this.saveData} current="personal-budget" prev="/calculator/demographics" next="/calculator/relocation-budget"/>
             </div>
